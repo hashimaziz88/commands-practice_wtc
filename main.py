ad675218ece66ce5286585c2f9623e6f8c0e903e
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-09-12 15:39:28 +0200

    commits code H

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def add():
 def multiply():
     ...
 
-def subtract():
-    ...
+def subtract(x, y):
+    return f"The subtraction of {x} - {y} = {x - y} "
+    
 
-def divide():
-    ...
+def divide(x, y):
+
+    return f"The division of {x} // {y} = {x // y} "
